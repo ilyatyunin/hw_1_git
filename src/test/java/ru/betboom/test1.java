2701000197ddb310d2d7c97c174199732b4f0a41
Тюнин Илья Сергеевич <is_tyunin@bbgroup.local>
2022-09-17 16:59:23 +0300

    first_test

---
+++ src/test/java/ru/betboom/test1.java
@@ -0,0 +1,12 @@
+package ru.betboom;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    @Test
+    void exampleTest() {
+        Assertions.assertTrue( 2>1);
+
+}
+}
